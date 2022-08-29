fdcb340956c38ab6497f331e90542c34990e5a64
creativoma <ma.marianoalvarez@gmail.com>
2022-08-29 22:38:10 +0200

    Modals Finished

--- src/utils/enviar.php
+++ src/utils/enviar.php
@@ -1,10 +1,18 @@
 <?php
-$to = "emesmediacontact@gmail.com";
+$result
+$to = "ma.marianoalvarez@gmail.com";
 $subject = "Mensaje desde EMESMEDIA";
 $txt = "Nombre: ".$_POST['name']."\n"."Teléfono: ".$_POST['tel']."\n"."Mail: ".$_POST['email']."\n"."Organitation: ".$_POST['organizacion']."\n"."Mensaje: ".$_POST['mensaje'];
-$headers = "From: emesmediacontact@gmail.com" . "\r\n" .
-"CC: emesmediacontact@gmail.com";
+$headers = "From: ma.marianoalvarez@gmail.com" . "\r\n" .
+"CC: ma.marianoalvarez@gmail.com";
 
-mail($to,$subject,$txt,$headers);
+$result = mail($to,$subject,$txt,$headers);
 header("location:index.html");
+
+// check result
+if(!$result){
+    echo "Error al enviar el mensaje";
+}else{
+    echo "Mensaje enviado correctamente";
+}
 ?>
