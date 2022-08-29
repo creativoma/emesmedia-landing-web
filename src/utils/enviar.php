9d791c8f30a3b8fe466034b6fbab13989028c3f1
creativoma <ma.marianoalvarez@gmail.com>
2022-08-30 00:41:36 +0200

    Final commits

--- src/utils/enviar.php
+++ src/utils/enviar.php
@@ -1,18 +1,16 @@
 <?php
-$result
-$to = "ma.marianoalvarez@gmail.com";
+$result = '';
+$to = "emesmediacontact@gmail.com"; // El correo a donde va el formulario
 $subject = "Mensaje desde EMESMEDIA";
 $txt = "Nombre: ".$_POST['name']."\n"."Teléfono: ".$_POST['tel']."\n"."Mail: ".$_POST['email']."\n"."Organitation: ".$_POST['organizacion']."\n"."Mensaje: ".$_POST['mensaje'];
-$headers = "From: ma.marianoalvarez@gmail.com" . "\r\n" .
-"CC: ma.marianoalvarez@gmail.com";
+$headers = "From: ".$_POST['email']. "\r\n" .  
+"CC: emesmediacontact@gmail.com"; // El correo de la persona que va en Copia
 
 $result = mail($to,$subject,$txt,$headers);
-header("location:index.html");
 
-// check result
-if(!$result){
-    echo "Error al enviar el mensaje";
+if($result){
+    header("location:/enviado.html");
 }else{
-    echo "Mensaje enviado correctamente";
+    header("location:/no-enviado.html");
 }
 ?>
